68175db4e63534536f6a943fa07ceb95f9ff1334
Jorge Mauro <jmaurodev@gmail.com>
2020-11-21 18:55:05 -0300

    Profile page creation

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Profile = () => (
+  <div>
+    <h1>Jorge Mauro de Lemos Moreira Junior</h1>
+    <h3>Desenvolvedor de Software</h3>
+  </div>
+)
+
+export default Profile
